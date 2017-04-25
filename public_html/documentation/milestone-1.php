6dd89668bbed463b7f783d6b5bad40d88cb81275
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-04-25 09:06:38 -0600

    Outline of milestone one - includes purpose, audience, and goal.

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+
+		<title>littleloveprint</title>
+	</head>
+	<body>
+		<header>
+			<h1>littleloveprint</h1>
+		</header>
+		<main>
+			<h2>Purpose, Audience, and Goal</h2>
+			<h3>Purpose:</h3>
+			<p>The purpose of my PWP is to create an informative, visually appealing site to showcase my talents and abilities in order to procure freelance or permanent work.</p>
+			<h3>Audience:</h3>
+			<p>The majority of users visiting my site will be potential clients and/or employers. These people are looking for someone with my skill set.</p>
+			<h3>Goal:</h3>
+			<p>The number one goal of this project is to attract freelance clients.</p>
+			<h2>Persona</h2>
+			<p></p>
+			<h2>Use Case</h2>
+			<p></p>
+		</main>
+	</body>
+</html>
