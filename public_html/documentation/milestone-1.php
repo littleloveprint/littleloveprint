7388df50acee0fdaa3817fc02430045388e8a6de
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-04-26 16:36:45 -0600

    Added user story, grammar check.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -10,13 +10,15 @@
 			<h1>littleloveprint</h1>
 		</header>
 		<main>
-			<h2>Purpose, Audience, and Goal</h2>
+			<h2>Purpose, Audience, Goal and User Story</h2>
 			<h3>Purpose:</h3>
 			<p>The purpose of my PWP is to create an informative, visually appealing site to showcase my talents and abilities in order to procure freelance or permanent work.</p>
 			<h3>Audience:</h3>
 			<p>The majority of users visiting my site will be potential clients and/or employers. These people are looking for someone with my skill set.</p>
 			<h3>Goal:</h3>
 			<p>The number one goal of this project is to attract freelance clients.</p>
+			<h3>User Story</h3>
+			<p>As a user, Nakeesia needs to determine whether or not I am the right developer for her project.</p>
 			<h2>Persona</h2>
 			<p><strong>Name: </strong>Nakeesia Kann<br><strong>Age: </strong>25</p>
 			<p><strong>Profession: </strong>Personal fitness trainer, food and beer enthusiast, social butterfly.</p>
@@ -25,7 +27,7 @@
 			<p><strong>Frustrations and Needs: </strong>Nakeesia is young and has a very active social life. She doesn't want her personal training business to suffer because she doesn't put in enough marketing hours, but she doesn't want to put her social life on hold either. She really wants a website where people can see her accreditations, her bio, and book appointments. She feels this will help her spend less time on the phone answering questions, so she can spend that time with her friends and family. She has no web development experience, but she knows she needs a site that will be appealing to her social circle (popular site > high traffic > more clients).</p>
 			<p><strong>Goals: </strong>Nakeesia wants to hire a web developer with graphic design skills to create a visually stimulating and informative website for her.</p>
 			<h2>Use Case</h2>
-			<p>Nakeesia is at dinner with a group of her friends at 7:00pm on a Friday night. They are all talking about their plans for the Summer, and Nakeesia mentions she can't make solid plans until she gets more clients. She really wants a website to do so, but she doesn't know anything about web design. Her friend hands her a business card and mentions she met a web developer when she was at Marble the week prior. Nakeesia pulls out her iPhone 7, and goes to the website on her phone. She loves the look and ease of the site, and she checks out a few items from the Portfolio page. She checks out a few of the social media accounts listed on the site and likes what she sees. Everything seems professional, yet friendly, warm, and genuine. She checks out the GitHub profile, but doesn't understand what she's looking at. She assumes it's the code that makes websites, so she returns to the site (which is accessible through GitHub), and she fills out a contact form, in hopes of hiring Lea McDuffie to build her personal fitness website.</p>
+			<p>Nakeesia is at dinner with a group of her friends at 7:00pm on a Friday night. They are all talking about their plans for the Summer, and Nakeesia mentions she can't make solid plans until she gets more clients. She really wants a website to do so, but she doesn't know anything about web design. Her friend hands her a business card and mentions she met a web developer when she was at Marble the week prior. Nakeesia pulls out her iPhone 7 and goes to the website on her phone. She loves the look and ease of the site, and she checks out a few items from the Portfolio page. She visits a few of the social media accounts listed on the site and likes what she sees there as well. Everything seems professional, yet friendly, warm, and genuine. She checks out the GitHub profile, but doesn't understand what she's looking at. She assumes it's the code that makes websites, so she returns to the site (which is accessible through GitHub), and she fills out a contact form in hopes of hiring Lea McDuffie to build her personal fitness website.</p>
 		</main>
 	</body>
 </html>
