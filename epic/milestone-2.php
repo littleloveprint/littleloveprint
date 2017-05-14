f1cc964ca913c3163ca9b913c4a20a15ed822966
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-05-14 16:00:12 -0600

    Final changes to pwp milestone 2a!

--- epic/milestone-2.php
+++ epic/milestone-2.php
@@ -11,16 +11,14 @@
 		<h2><strong>411:</strong></h2>
 		<p>This section will have information on my professional history, showcase my skillset and personality as well as highlight the programming languages I am most familiar with. My hope is to attract the type of employer I work best with through the professional yet fun and creative look and feel of my web app.</p>
 		<h2><strong>Portfolio:</strong></h2>
-		<p>My hopes are to have three separate, responsive image galleries.
+		<p>My portfolio will consist of two sub sections. The web development section will showcase current websites I am working on or have recently completed. The graphic design section will have a few graphics I've created such as logo designs, infographics and flyers.
 		<ul>
-			<li>Graphics (logos, print designs, promotional GIFs)</li>
-			<li>Web (websites I've created)</li>
-			<li>Photography (staging, styling, photography, and editing)</li>
+			<li>Web Development (websites I've created)</li>
+			<li>Graphic Design (logos, print designs, promotional GIFs)</li>
 		</ul>
-		Included underneath each gallery will be a short outline of my role(s) in each project.</p>
+		Included underneath each sub section will be a short outline of my role(s) in each project.</p>
 		<h2><strong>Contact:</strong></h2>
-		<p>This section will list my professional email address, my universal at handle (social links also listed at top of page), and a contact form.</p>
-		<p><strong></strong></p>
+		<p>This section will list my professional email address, my universal at handle (social links also visible), and a contact form.</p>
 		<img src="img/mobile.png" alt="Mobile Wireframe"><br>
 		<img src="img/desktop.png" alt="Desktop Wireframe">
 	</body>
