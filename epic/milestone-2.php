d45c64ac6b03e295cedc81786106698d2b15bf00
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-05-15 13:19:05 -0600

    Paragraph edit.

--- epic/milestone-2.php
+++ epic/milestone-2.php
@@ -11,12 +11,8 @@
 		<h2><strong>411:</strong></h2>
 		<p>This section will have information on my professional history, showcase my skillset and personality as well as highlight the programming languages I am most familiar with. My hope is to attract the type of employer I work best with through the professional yet fun and creative look and feel of my web app.</p>
 		<h2><strong>Portfolio:</strong></h2>
-		<p>My portfolio will consist of two sub sections. The web development section will showcase current websites I am working on or have recently completed. The graphic design section will have a few graphics I've created such as logo designs, infographics and flyers.
-		<ul>
-			<li>Web Development (websites I've created)</li>
-			<li>Graphic Design (logos, print designs, promotional GIFs)</li>
-		</ul>
-		Included underneath each sub section will be a short outline of my role(s) in each project.</p>
+		<p>My portfolio will consist of two sub sections. The web development section will showcase current websites I am working on or have recently completed. The graphic design section will have a few graphics I've created such as logo designs, infographics and flyers.</p>
+		<p>Included underneath each sub section will be a short outline of my role(s) in each project.</p>
 		<h2><strong>Contact:</strong></h2>
 		<p>This section will list my professional email address, my universal at handle (social links also visible), and a contact form.</p>
 		<img src="img/mobile.png" alt="Mobile Wireframe"><br>
