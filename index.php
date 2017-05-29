1e892610594646a9ea3d4d03b3f4db91c0b33614
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-05-29 11:08:03 -0600

    Navbar placement good.

--- index.php
+++ index.php
@@ -11,16 +11,18 @@
 		<!-- Optional theme -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous" />
 
+		<!-- Font Awesome -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+		<!-- My Custom CSS -->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
 		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 		<!--[if lt IE 9]>
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
-
-		<!-- Lea's Custom CSS Below -->
-
-
 
 		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
@@ -32,18 +34,43 @@
 	</head>
 	<body class="sfooter">
 		<div class="sfooter-content">
-			<!-- Nav Bar -->
-			<ul>
-				<li><a href="index.php">Home</a> </li>
-				<li><a href="411.php">411</li>
-				<li><a href="portfolio.php">Portfolio</a> </li>
-				<li><a href="contact.php">Contact</a> </li>
-			</ul>
+
+			<!-- Navigation bar -->
+			<nav class="navbar navbar-default">
+				<div class="container">
+
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-left" href="#"><img src="img/1.png" alt="littleloveprint logo"/></a>
+					</div>
+
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav navbar-right">
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+									aria-expanded="false">Navigation Station<span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a href="https://www.bosquebrewing.com" target="_blank">HOME</a></li>
+									<li><a href="https://www.marblebrewery.com" target="_blank">411</a></li>
+									<li><a href="https://www.lacumbrebrewing.com" target="_blank">PORTFOLIO</a>
+									</li>
+									<li role="separator" class="divider"></li>
+									<li><a href="https://www.littleloveprint.io">CONTACT</a></li>
+								</ul>
+					</div><!-- /.navbar-collapse -->
+				</div><!-- /.container-fluid -->
+			</nav><!-- /.navigation bar -->
 
 			<!-- Header -->
 			<header>
-				<img src="img/litleloveprint-logo.png" alt="littleloveprint logo"/>
-				<h2>Software Development + Graphic Design</h2>
+				<h2>SOFTWARE | GRAPHICS</h2>
 			</header>
 		</div>
 	</body>
