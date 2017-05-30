6bab7a5920d9da16875b90dbf1b8b2324b5bf02c
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-05-30 11:54:37 -0600

    REVAMP

--- index.php
+++ index.php
@@ -1,117 +1,82 @@
 <!DOCTYPE html>
 <html lang="en-US" class="no-js">
 	<head>
-		<meta name="google-site-verification" content="2wOAvvalmtOK2T875j4nX0zoB8pB7M7KNN3ULO9uwkM"/>
-		<meta charset="utf-8"/>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta charset="utf-8">
+		<title>LEA MCDUFFIE @LITTLELOVEPRINT</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="A PORTFOLIO WEBSITE FOR LEA MCDUFFIE">
+		<meta name="author" content="LEA MCDUFFIE @littleloveprint lea@littleloveprint.io">
 
-		<!-- CSS -->
-		<link rel="stylesheet" href="assets/css/bootstrap.css">
-		<link rel="stylesheet" href="assets/css/font-awesome.css">
-		<link rel="stylesheet" href="assets/css/flexslider.css">
-		<link rel="stylesheet" href="assets/css/styles.css">
-		<link rel="stylesheet" href="css/style.css" type="text/css">
+		<!-- LLP Favicon-->
+		<link rel="shortcut icon"
+				href="img/llpfavicon.png">
 
-		<!-- Font Awesome -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<!-- Bootstrap CSS Files -->
+		<link href="css/bootstrap.css" rel="stylesheet">
 
-		<!-- Google -->
-		<link href='http://fonts.googleapis.com/css?family=Raleway:400,300,200,500,600,700' rel='stylesheet'
-				type='text/css'>
+		<!-- Custom Fonts Setup via Font-face CSS3 -->
+		<link href="fonts/fonts.css" rel="stylesheet">
 
-		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
-		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!-- CSS files for plugins -->
+		<link href="css/pace.preloader.css" rel="stylesheet">
+		<link href="css/owl.carousel.css" rel="stylesheet">
+		<link href="css/owl.theme.css" rel="stylesheet">
+		<link href="css/venobox.css" rel="stylesheet"/>
+		<link href="css/animate.css" rel="stylesheet">
+		<link href="css/jquery.tweet.css" rel="stylesheet"/>
+		<link href="css/slidingmenu.css" rel="stylesheet">
+		<link href="css/jquery.bxslider.css" rel="stylesheet"/>
+		<link href="css/venobox.css" rel="stylesheet"/>
+		<link href="css/animate.css" rel="stylesheet">
+		<link href="css/portfolio.css" rel="stylesheet"/>
+		<link href="css/overlay.css" rel="stylesheet">
+		<link href="css/toppanel.css" rel="stylesheet">
+		<link href="css/intro.css" rel="stylesheet">
+
+		<!-- Main Styles -->
+		<link href="css/main.css" rel="stylesheet">
+
+		<!-- Responsive Styles -->
+		<link href="css/main-responsive.css" rel="stylesheet">
+
+		<!-- Managing Color Presets -->
+		<link rel="stylesheet/less" type="text/css"
+				href="less/color-crimson.less">
+
+		<!-- LESS JS Engine-->
+		<script src="less/less-1.5.0.min.js"></script>
+
+		<!-- Google Fonts-->
+		<link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700,900%7COpen+Sans:400,300,700,800%7CMontserrat:400,700' rel='stylesheet' type='text/css'>
+
+		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<script src="bootstrap/js/html5shiv.js"></script>
+		<script src="bootstrap/js/respond.min.js"></script>
 		<![endif]-->
-
-		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
-
-		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-		<!-- jQuery -->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-
-		<!-- jQuery Form, Additional Methods, Validate -->
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/jquery.validate.min.js"></script>
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/additional-methods.min.js"></script>
-
-		<script src="assets/js/libs/respond.min.js"></script>
-		<script type="text/javascript" src="assets/js/libs/modernizr.min.js"></script>
-
-		<!-- Your JavaScript Form Validator -->
-		<script src="js/form-validate.js"></script>
-
-		<!-- Google reCAPTCHA -->
-		<script src='https://www.google.com/recaptcha/api.js'></script>
-
-		<!-- Scrolling Nav JavaScript -->
-		<script src="js/jquery.easing.min.js"></script>
-		<script src="js/scrolling-nav.js"></script>
-
-		<title>littleloveprint</title>
 	</head>
 
-	<!-- Body -->
-	<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
-		<body data-spy="scroll" data-target="#main-nav" data-offset="200">
+	<body>
 
-				<!-- Page Preloader -->
-				<div id="page-loader"><span class="page-loader-gif"></span></div>
+		<!-- Mobile Only Navigation -->
+		<nav class="menu" id="sm">
+			<div class="sm-wrap">
+				<a href="index.php"><img title="littleloveprint" alt="logo" class="logo" src="img/1.png"/></a>
+				<i class="icon-remove menu-close"></i>
+				<a class="scroll" href="#intro">HOME</a>
+				<a class="scroll" href="#about">411</a>
+				<a class="scroll" href="#portfolio">PORTFOLIO</a>
+				<a class="scroll" href="#contact">CONTACT</a>
+			</div>
 
-					<!-- Navigation Station -->
-			<div id="main-nav" class="navbar navbar-fixed-top">
-				<div class="container">
+			<!-- Navigation Trigger Button -->
+			<div id="sm-trigger"></div>
+		</nav><!-- /Mobile Only  Nav -->
 
-					<div class="navbar-header">
+		<!-- Top Sliding Panel -->
+		<div id="toppanel" class="toppanel">
 
-						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#site-nav">
-							<span class="icon-bar"></span><span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-						<a class="navbar-brand scrollto" href="#home"><img src="img/1.png" alt="logo">LEA MCDUFFIE<br>SOFTWARE | GRAPHICS</a>
-					</div>
+			<div class="toppanel-container">
+				<div class="toppanel-wrapper white-bg">
 
-					<div id="site-nav" class="navbar-collapse collapse">
-						<ul class="nav navbar-nav navbar-right">
-							<li class="sr-only">
-								<a href="#home" class="scrollto">HOME</a>
-							</li>
-							<li>
-								<a href="#about" class="scrollto">411</a>
-							</li>
-							<li>
-								<a href="#portfolio" class="scrollto">PORTFOLIO</a>
-							</li>
-							<li>
-								<a href="#contact" class="scrollto">CONTACT</a>
-							</li>
-						</ul>
-					</div><!-- /navbar-collapse -->
-				</div><!-- /container -->
-			</div><!-- /nav -->
-
-			<!-- Header -->
-			<header id="home">
-				<div class="container text-center">
-					<p>WORDS ON WORDS ON WORDS.</p>
-				</div>
-			</header><!-- /header -->
-
-			<!-- About -->
-			<section id="about" class="add-padding bg-color2">
-				<div class="row">
-					<div class="col-sm-6 scrollimation fade-right d1">
-						<img class="img-responsive polaroid src=img/vert.jpg" alt="tea and code"/>
-					</div>
-				</div>
-			</section>
-			</header>
-	</body>
-</html>
+					<div class="row">
