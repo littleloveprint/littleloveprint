76591f6715e495cce14d1bf753e6d84e9682b182
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-05-30 15:16:33 -0600

    HOME

--- index.php
+++ index.php
@@ -80,3 +80,63 @@
 				<div class="toppanel-wrapper white-bg">
 
 					<div class="row">
+						<article class="col-md-6 text-left">
+							<img alt="Lea McDuffie" title="littleloveprint" class="img-responsive"
+								  src="/img/llpblackicon.png"/>
+							<h6 class="brand dark"><span>LITTLELOVEPRINT</span></h6>
+						</article>
+
+						<article class="col-md-6 text-right">
+							<ul class="top-nav top-nav-dark-text">
+								<li><a href="#intro" class="scroll highlighted">HOME</a></li>
+								<li><a href="#about" class="scroll">411</a></li>
+								<li><a href="#portfolio" class="scroll">PORFOLIO</a></li>
+								<li><a href="#contact" class="scroll">CONTACT</a></li>
+							</ul>
+						</article>
+
+					</div><!-- /row -->
+				</div><!-- /toppanel-wrapper -->
+			</div><!-- /toppanel-container -->
+		</div><!-- /Top Sliding Panel -->
+
+		<!-- Master Wrap -->
+		<section id="mastwrap" class="sliding">
+
+			<!-- masthead-section -->
+			<header id="intro-header" class="intro-header">
+				<div class="row">
+					<article class="col-md-3 text-left">
+						<a href="index.php"><img alt="LEA MCDUFFIE" title="LEA MCDUFFIE" class="home-logo img-responsive" src="/img/llpblackicon.png"/></a>
+					</article>
+					<article class="col-md-9 text-right">
+						<ul class="intro-nav  intro-nav-white-text">
+							<li><a href="#" class="trigger-toppanel"><img alt="littleloveprint" title="littleloveprint" src="/img/menu-white.png"/></a>
+							</li>
+						</ul>
+					</article>
+				</div>
+			</header>
+			<!-- /masthead-section -->
+
+			<!-- HOME -->
+			<section id="intro" class="intro intro-01 text-center">
+
+				<div class="overlay dark-overlay full-height full-width"></div>
+
+				<div class="valign">
+					<h3 class="white">LITTLELOVEPRINT</h3>
+					<h1 class="white"><span>SOFTWARE | GRAPHICS</span></h1>
+				</div><!-- /valign -->
+
+				<div class="mouse-icon-wrap full-width">
+					<a class="scroll" href="#about">
+						<div class="mouse-icon white hidden-xs" style="opacity: 1;">
+							<div class="wheel"></div>
+						</div>
+					</a>
+				</div>
+
+			</section>
+			<!-- /HOME -->
+
