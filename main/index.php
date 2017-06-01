a001fd25ef671cb0a797c9eea4647af675f15ee8
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-06-01 10:22:06 -0600

    Accidentally removed my contact form jquery and javascript.

--- main/index.php
+++ main/index.php
@@ -97,6 +97,20 @@
 		<script src="javascripts/toppanel.js"></script>
 		<script src="javascripts/main.js"></script>
 
+		<!-- jQuery -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+
+		<!-- jQuery Form, Additional Methods, Validate -->
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/additional-methods.min.js"></script>
+
+		<!-- Your JavaScript Form Validator -->
+		<script src="js/form-validate.js"></script>
+
+		<!-- Google reCAPTCHA -->
+		<script src='https://www.google.com/recaptcha/api.js'></script>
+
 	</head>
 
 	<body>
