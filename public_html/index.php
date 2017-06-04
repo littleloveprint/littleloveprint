4343197bbdc3a86c3e7b2019a32969718cd01116
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-06-04 08:36:24 -0600

    Nav bar ok.

--- public_html/index.php
+++ public_html/index.php
@@ -1,59 +1,109 @@
 <!DOCTYPE html>
-<html>
+<html xmlns="http://www.w3.org/1999/html">
 	<head>
 
-		<!-- A PERSONAL WEBSITE -->
-		<!-- @author LEA MCDUFFIE lea@littleloveprint.io-->
-		<!-- @version 3.0 -->
-
 		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<title>littleloveprint</title>
-		<meta name="description" content="">
-		<meta name="viewport" content="width=device-width">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="description" content="Portfolio website for Lea McDuffie">
+		<meta name="author" content="Lea McDuffie">
+		<meta name="version" content="4.0">
+
+		<!-- BOOTSTRAP -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- OPTIONAL THEME -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- FONT AWESOME -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
 		<!-- GOOGLE FONT -->
 		<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
 
-		<!-- BOOTSTRAP -->
-		<link rel="stylesheet" href="css/bootstrap.min.css">
-
 		<!-- STYLES -->
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 
+		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 		<!--[if lt IE 9]>
-		<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-		<script>window.html5 || document.write('<script src="js/vendor/html5shiv.js"><\/script>')</script>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
 
-		<!-- javascript -->
-		<script type="text/javascript" src="rellax.js"></script>
-		<script>var rellax = new Rellax('.rellax', {
-			});
-		</script>
-
-		<!-- jQuery -->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+		<!-- jQuery v3.0 -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js" type="text/javascript"></script>
 
 		<!-- jQuery Form, Additional Methods, Validate -->
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/jquery.validate.min.js"></script>
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/additional-methods.min.js"></script>
 
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
 		<!-- Your JavaScript Form Validator -->
 		<script src="js/form-validate.js"></script>
 
+		<title>littleloveprint</title>
+
 		<!-- Google reCAPTCHA -->
 		<script src='https://www.google.com/recaptcha/api.js'></script>
+
+		<!-- Scrolling Nav JavaScript -->
+		<script src="js/jquery.easing.min.js"></script>
+		<script src="js/scrolling-nav.js"></script>
 	</head>
 
-	<body>
-		<div class="grid">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-offset-1 col-md-2">
-						<div class="grid-line"></div>
+	<body class="sfooter">
+		<div class="sfooter-content">
+
+			<header>
+				<!-- NAVBAR -->
+				<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+					<div class="container">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<div class="small-logo-container">
+								<img class= "image-responsive" src="img/6.png" alt="logo">
+							</div>
+						</div>
+						<div class="navbar-collapse collapse">
+
+							<ul class="nav navbar-nav navbar-right">
+								<li class="active"><a href="#">HOME</a></li>
+								<li><a href="#about">411</a></li>
+								<li><a href="#portfolio">PORTFOLIO</a></li>
+								<li><a href="#contact">CONTACT</a></li>
+							</ul>
+						</div>
 					</div>
+				</div>
+
+				<div class="container-fluid big-logo-row">
+					<div class="container">
+						<div class="row">
+							<div class="col-xs-12 big-logo-container">
+								<img class= "image-responsive" src="" alt="logo">
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
+
+						<section id="intro" class="intro intro-01 text-center">
+						<div class="grid">
+							<div class="container">
+								<div class="row">
+									<div class="col-md-offset-1 col-md-2">
+										<div class="grid-line"></div>
+									</div>
 					<div class="col-md-2">
 						<div class="grid-line"></div>
 					</div>
@@ -75,34 +125,33 @@
 		</div>
 
 		<main class="main">
-			<header>
 				<div class="container">
 					<div class="row">
 						<div class="col-md-offset-1 col-md-11">
 							<a href="https://littleloveprint.io">
-								<img class="img-responsive" src="img/logo.png" alt="logo">
 								<h1>➔</h1>
 							</a>
 							<div class="white-block"></div>
 						</div>
 					</div>
+
+					<section id="about" class="page page-section about">
 					<div class="title">
 						<div class="row">
 							<div class="col-md-offset-1 col-md-7">
 								<h1>411<br><span>LEA</span></h1>
-								<p>Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi amaranth water spinach avocado daikon napa cabbage asparagus winter purslane kale. Celery potato scallion desert raisin horseradish spinach carrot soko. Lotus root water spinach fennel kombu maize bamboo shoot green bean swiss chard seakale pumpkin onion chickpea gram corn pea. Brussels sprout coriander water chestnut gourd swiss chard wakame kohlrabi beetroot carrot watercress. Corn amaranth salsify bunya nuts nori azuki bean chickweed potato bell pepper artichoke.</P>
-							</div>
-							<div class="col-md-offset-6 col-md-7">
-								<h1><span>LITTLELOVEPRINT</span></h1>
-								<p>Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi amaranth water spinach avocado daikon napa cabbage asparagus winter purslane kale. Celery potato scallion desert raisin horseradish spinach carrot soko. Lotus root water spinach fennel kombu maize bamboo shoot green bean swiss chard seakale pumpkin onion chickpea gram corn pea. Brussels sprout coriander water chestnut gourd swiss chard wakame kohlrabi beetroot carrot watercress. Corn amaranth salsify bunya nuts nori azuki bean chickweed potato bell pepper artichoke.</p><br>
+								<p>Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi amaranth water spinach avocado daikon napa cabbage asparagus winter purslane kale. Celery potato scallion desert raisin horseradish spinach carrot soko. Lotus root water spinach fennel kombu maize bamboo shoot green bean swiss chard seakale pumpkin onion chickpea gram corn pea. Brussels sprout coriander water chestnut gourd swiss chard wakame kohlrabi beetroot carrot watercress. Corn amaranth salsify bunya nuts nori azuki bean chickweed potato bell pepper artichoke.</p><br><br><br>
 							</div>
 						</div>
 					</div>
-					<div class="row">
+					</section>
+
+					<section id="portfolio" class="page page-section portfolio">
+						<div class="row">
 						<div class="col-md-offset-1 col-md-6">
 							<h1>PORTFOLIO</h1>
 							<h2>WEB</h2>
-							<p>Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi amaranth water spinach avocado daikon napa cabbage asparagus winter purslane kale. Celery potato scallion desert raisin horseradish spinach carrot soko. Lotus root water spinach fennel kombu maize bamboo shoot green bean swiss chard seakale pumpkin onion chickpea gram corn pea. Brussels sprout coriander water chestnut gourd swiss chard wakame kohlrabi beetroot carrot watercress. Corn amaranth salsify bunya nuts nori azuki bean chickweed potato bell pepper artichoke.</p>
+							<p>Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi amaranth water spinach avocado daikon napa cabbage asparagus winter purslane kale. Celery potato scallion desert raisin horseradish spinach carrot soko. Lotus root water spinach fennel kombu maize bamboo shoot green bean swiss chard seakale pumpkin onion chickpea gram corn pea. Brussels sprout coriander water chestnut gourd swiss chard wakame kohlrabi beetroot carrot watercress. Corn amaranth salsify bunya nuts nori azuki bean chickweed potato bell pepper artichoke.</p><br><br><br>
 						</div>
 					</div>
 					<div class="container">
@@ -125,7 +174,7 @@
 					<div class="row">
 						<div class="col-md-offset-3 col-md-6">
 							<h2>MOBILE</h2>
-							<p>Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi amaranth water spinach avocado daikon napa cabbage asparagus winter purslane kale. Celery potato scallion desert raisin horseradish spinach carrot soko. Lotus root water spinach fennel kombu maize bamboo shoot green bean swiss chard seakale pumpkin onion chickpea gram corn pea. Brussels sprout coriander water chestnut gourd swiss chard wakame kohlrabi beetroot carrot watercress. Corn amaranth salsify bunya nuts nori azuki bean chickweed potato bell pepper artichoke.</p>
+							<p>Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi amaranth water spinach avocado daikon napa cabbage asparagus winter purslane kale. Celery potato scallion desert raisin horseradish spinach carrot soko. Lotus root water spinach fennel kombu maize bamboo shoot green bean swiss chard seakale pumpkin onion chickpea gram corn pea. Brussels sprout coriander water chestnut gourd swiss chard wakame kohlrabi beetroot carrot watercress. Corn amaranth salsify bunya nuts nori azuki bean chickweed potato bell pepper artichoke.</p><br><br><br>
 						</div>
 					</div>
 					<div class="container">
@@ -171,80 +220,6 @@
 								<div id="output-area"></div>
 							</div>
 						</div>
-			</header>
-
-			<section class="section san-francisco">
-				<div class="absolute">
-					<div class="container">
-						<div class="row">
-							<div class="col-md-offset-1 col-md-11">
-								<h2 class="rellax" data-rellax-speed="-3">CUSTOM</h2>
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<div class="absolute above">
-					<div class="container">
-						<div class="row">
-
-							<div class="col-md-offset-1 col-md-2">
-								<div class="lg-green rellax"
-									  data-rellax-speed="2"
-									  style="transform: rotate(45deg);">
-								</div>
-								<div class="sm-green rellax"
-									  data-rellax-speed="5"
-									  style="transform: rotate(45deg);">
-								</div>
-							</div>
-
-							<div class="col-md-offset-2 col-md-1">
-								<div class="sm-purple rellax"
-									  data-rellax-speed="2"
-									  style="transform: rotate(45deg);">
-								</div>
-							</div>
-
-							<div class="col-md-offset-3 col-md-1">
-								<div class="xs-green rellax"
-									  data-rellax-speed="5"
-									  style="transform: rotate(45deg);">
-								</div>
-								<div class="md-green rellax"
-									  data-rellax-speed="1"
-									  style="transform: rotate(45deg);">
-								</div>
-							</div>
-
-							<div class="col-md-1">
-								<div class="lg-purple rellax"
-									  data-rellax-speed="3"
-									  style="transform: rotate(45deg);">
-								</div>
-							</div>
-
-						</div>
-					</div>
-				</div>
-
-
-				<div class="container">
-
-					<div class="row">
-						<div class="col-md-offset-1 col-md-5">
-							<div class="date">
-								DEVELOPMENT—
-							</div>
-						</div>
-						<div class="col-md-offset-1 col-md-4">
-							<div class="temp">
-								BRAND
-							</div>
-						</div>
-					</div>
-				</div>
-			</section>
 
 			<section class="section">
 				<div style="margin-top: 200px">
@@ -303,7 +278,8 @@
 				</div>
 			</section>
 
-						<div class="row">
+						<section id="contact" class="page page-section contact">
+							<div class="row">
 							<div class="col-md-offset-5 col-md-6">
 								<h1>CONTACT</h1>
 							</div>
