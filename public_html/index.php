505442b7b566fe715c860f3b7dc99b7acd0c8d70
littleloveprint@gmail.com <littleloveprint@gmail.com>
2017-06-06 13:28:04 -0600

    Navbar fits in mobile display :)

--- public_html/index.php
+++ public_html/index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/html">
+<html xmlns="http://www.w3.org/1999/html" lang="en">
 	<head>
 
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 		<meta name="description" content="Portfolio website for Lea McDuffie">
 		<meta name="author" content="Lea McDuffie">
 		<meta name="version" content="4.0">
@@ -56,41 +56,49 @@
 		<script src="js/scrolling-nav.js"></script>
 	</head>
 
-	<body class="sfooter">
+	<body class="sfooter" id="page-top" data-spy="scroll" data-targer=".navbar-fixed-top">
 		<div class="sfooter-content">
 
 			<header>
 
 				<!-- NAVBAR -->
-				<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-					<div class="container">
-						<div class="navbar-header">
+				<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+					<div class="container-fluid">
+
+						<!-- MOBILE DISPLAY -->
+						<div class="navbar-header page-scroll">
 							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
 								<span class="sr-only">Toggle navigation</span>
 								<span class="icon-bar"></span>
 								<span class="icon-bar"></span>
 								<span class="icon-bar"></span>
 							</button>
-							</div>
-						</div>
-						<div class="navbar-collapse collapse">
-							<div class="small-logo-container">
-							</div>
-
-							<ul class="nav navbar-nav navbar-right">
-								<li class="active"><a href="#">HOME</a></li>
-								<li><a href="#about">411</a></li>
-								<li><a href="#portfolio">PORTFOLIO</a></li>
-								<li><a href="#contact">CONTACT </a></li>
-							</ul>
-
-							<div class="navbar-header">
+							<a class="navbar-header page scroll" href="#page-top"></a>
 								<a class="navbar-left" href="#"><img src="img/2.png" alt="littleloveprint logo"/></a>
-							</div>
+						</div>
+
+						<!-- COLLECT NAV LINKS, FORMS, AND OTHER CONTENT FOR TOGGLING -->
+						<div class="collapse navbar-collapse navbar-ex1-collapse">
+							<ul class="nav navbar-nav navbar-right">
+								<li class="hidden">
+									<a class="page-scroll" href="#page-top"></a>
+								</li>
+								<li>
+									<a class="page-scroll" href="#home">HOME</a>
+								</li>
+								<li>
+									<a class="page-scroll" href="#about">411</a>
+								</li>
+								<li>
+									<a class="page-scroll" href="#portfolio">PORTFOLIO</a>
+								</li>
+								<li>
+									<a class="page-scroll" href="#contact">CONTACT</a>
+								</li>
+							</ul>
 						</div>
 					</div>
-				</div>
-
+				</nav>
 			</header>
 
 			<section id="intro" class="intro intro-01 text-left">
@@ -116,53 +124,59 @@
 					</div>
 				</div>
 
-				<div class="fixed-top rellax" data-rellax-speed="-2" style="transform: rotate(45deg);">
+					<div class="fixed-top rellax" data-rellax-speed="-2" style="transform: rotate(45deg);">
 				</div>
-
-				<main class="main">
-					<div class="container">
-						<div class="row">
-							<div class="col-md-offset-1 col-md-11">
-								<a href="https://littleloveprint.io">
-									<h1>➔</h1>
-								</a>
-								<div class="white-block"></div>
-							</div>
-						</div>
-
-						<section id="about" class="page page-section about">
-							<div class="title">
-								<div class="row">
-									<div class="col-md-offset-1">
-										<h1>411<br><span>LEA</span></h1>
-										<p>Lea was first introduced to coding during her sophomore year in college in 2007. She learned HTML, basic CSS, and she learned how to navigate Dreamweaver. Not having studied any other coding languages, but being skilled and experienced in graphic design, Lea started working on freelance projects designing logos and updating the front ends of websites.</p><br>
-										<p>After ten years of brand development, product management and web design experience, Lea decided it was time to update and advance her coding skills. Through courses taken with Codecademy, Lynda, Google's Nanodegree programs and a Fullstack Web Development bootcamp with the CNM STEMulus Center, Lea now has the following skills and abilities:</p><br>
-										<ul>
-											<li>ANDROID</li>
-											<li>ANGULAR</li>
-											<li>BOOTSTRAP</li>
-											<li>CSS</li>
-											<li>GIT</li>
-											<li>GITHUB</li>
-											<li>G SUITE</li>
-											<li>HTML</li>
-											<li>IOS</li>
-											<li>JAVASCRIPT</li>
-											<li>JQUERY</li>
-											<li>ILLUSTRATOR</li>
-											<li>INDESIGN</li>
-											<li>MICROSOFT OFFICE</li>
-											<li>PHOTOSHOP</li>
-											<li>MYSQL</li>
-											<li>PHP</li>
-											<li>WORDPRESS</li>
-										</ul><br>
-									</div>
+					<main class="main">
+						<div class="container pt-1 mb-3">
+							<div class="row">
+								<div class="col-md-offset-1 col-md-12">
+									<a href="#contact">
+										<h1>➔</h1>
+									</a>
+									<div class="white-block"></div>
 								</div>
 							</div>
-						</section>
+						</div>
+					</main>
+				</div>
 
-						<section id="portfolio" class="page page-section portfolio">
+		<section id="home" class="page page-section home">
+			<div class="row">
+				<div class="col-md-offset-1 col-md-12">
+				</div>
+			</div>
+		</section>
+
+				<section id="about" class="page page-section about">
+							<div class="row">
+								<div class="col-md-offset-1 col-md-12">
+									<h1>411<br><span>LEA</span></h1>
+									<p>Lea was first introduced to coding during her sophomore year in college in 2007. She learned HTML, basic CSS, and she learned how to navigate Dreamweaver. Not having studied any other coding languages, but being skilled and experienced in graphic design, Lea started working on freelance projects designing logos and updating the front ends of websites.</p><br>
+									<p>After working for ten years in brand development, product management and web design, Lea decided it was time to update and advance her coding skills. From prior education and experience, and after courses taken with Codecademy, Lynda, Google's Nanodegree programs and a Fullstack Web Dev bootcamp with the CNM STEMulus Center, some of Lea's skills include:</p><br>
+									<ul>
+										<li>ANDROID</li>
+										<li>ANGULAR</li>
+										<li>BOOTSTRAP</li>
+										<li>CSS</li>
+										<li>GIT</li>
+										<li>GITHUB</li>
+										<li>G SUITE</li>
+										<li>HTML</li>
+										<li>IOS</li>
+										<li>JAVASCRIPT</li>
+										<li>JQUERY</li>
+										<li>ILLUSTRATOR</li>
+										<li>INDESIGN</li>
+										<li>MICROSOFT OFFICE</li>
+										<li>PHOTOSHOP</li>
+										<li>MYSQL</li>
+										<li>PHP</li>
+										<li>WORDPRESS</li>
+								</div>
+							</div>
+				</section>
+
+				<section id="portfolio" class="page page-section portfolio">
 							<div class="row">
 								<div class="col-md-offset-7 col-md-6">
 									<h1>PORTFOLIO</h1>
@@ -172,18 +186,14 @@
 							</div>
 							<div class="container">
 								<div class="row">
-									<div class="col-md-3">
-										<img class="img-responsive img-circle" src="img/BarkParkz.png" alt="Bark Parkz">
+									<div class="col-md-4">
+										<img class="img-responsive" src="img/BarkParkz.png" alt="Bark Parkz">
 									</div>
-									<div class="col-md-3">
-										<img class="img-responsive img-circle" src="img/bosque.png" alt="Bosque">
+									<div class="col-md-4">
+										<img class="img-responsive" src="img/bosque.png" alt="Bosque">
 									</div>
-									<div class="col-md-3">
-										<img class="img-responsive img-circle" src="img/baesic.png" alt="Baesic">
-									</div>
-									<div class="col-md-3">
-										<img class="img-responsive img-circle" src="img/hippo.png" alt="HIPPO">
-										<br>
+									<div class="col-md-4">
+										<img class="img-responsive" src="img/baesic.png" alt="Baesic">
 									</div>
 								</div>
 							</div>
@@ -211,150 +221,137 @@
 								<div class="row">
 									<div class="col-md-offset-1 col-md-6">
 										<h2>BRAND</h2>
-										<p>Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi amaranth water spinach avocado daikon napa cabbage asparagus winter purslane kale. Celery potato scallion desert raisin horseradish spinach carrot soko. Lotus root water spinach fennel kombu maize bamboo shoot green bean swiss chard seakale pumpkin onion chickpea gram corn pea. Brussels sprout coriander water chestnut gourd swiss chard wakame kohlrabi beetroot carrot watercress. Corn amaranth salsify bunya nuts nori azuki bean chickweed potato bell pepper artichoke.</p>
+										<p>Graphic design > staging > photography > image manipulation > photo editing > logo design > merchandise design > styling > and more!</p>
 									</div>
 								</div>
+							</div>
+							<div class="container">
+								<div class="row">
+									<p>INSERT SLIDESHOW</p>
+								</div>
+							</div>
+
+							<div class="row">
+								<div class="col-md-6">
+									<div id="output-area"></div>
+								</div>
+							</div>
+				</section>
+
+		<section class="section">
+			<div style="margin-top: 200px">
+				<div class="container">
+
+					<section class="section">
+							<div class="absolute above" style="pointer-events: none;">
 								<div class="container">
 									<div class="row">
-										<div class="col-md-3">
-											<img class="img-responsive" src="img/placeholder0.png" alt="image">
+
+										<div class="col-md-offset-1 col-md-2">
+											<div class="bt-green rellax" data-rellax-speed="5">
+											</div>
 										</div>
-										<div class="col-md-3">
-											<img class="img-responsive" src="img/placeholder0.png" alt="image">
+											<div class="col-md-offset-0 col-md-1">
+												<div class="md-lea rellax" data-rellax-speed="2">
+
+												</div>
+											</div>
+										<div class="col-md-offset-5 col-md-1">
+											<div class="md-twitter rellax" data-rellax-speed="1" style="transform: rotate(45deg);">
+												</div>
 										</div>
-										<div class="col-md-3">
-											<img class="img-responsive" src="img/placeholder0.png" alt="image">
-										</div>
-										<div class="col-md-3">
-											<img class="img-responsive" src="img/placeholder0.png" alt="image">
+
+										<div class="col-md-1">
+											<div class="md-facebook rellax" data-rellax-speed="3" data-rellax-percentage="0.5" style="transform: rotate(45deg);">
+
+											</div>
 										</div>
 									</div>
 								</div>
+							</div>
 
+						<div class="copy" style="margin-top: 200px;margin-bottom: 500px">
+							<div class="container">
 								<div class="row">
-									<div class="col-md-6">
-										<div id="output-area"></div>
+									<div class="col-md-offset-1 col-md-6">
 									</div>
 								</div>
-
-								<section class="section">
-									<div style="margin-top: 200px">
-										<div class="container">
-
-											<section class="section">
-
-												<div class="absolute above" style="pointer-events: none;">
-													<div class="container">
-														<div class="row">
-
-															<div class="col-md-offset-1 col-md-2">
-																<div class="bt-green rellax" data-rellax-speed="5">
-																</div>
-															</div>
-
-															<div class="col-md-offset-0 col-md-1">
-																<div class="md-lea rellax" data-rellax-speed="2">
-
-																</div>
-															</div>
-															<div class="col-md-offset-5 col-md-1">
-																<div class="md-twitter rellax" data-rellax-speed="1" style="transform: rotate(45deg);">
-
-																</div>
-															</div>
-
-															<div class="col-md-1">
-																<div class="md-facebook rellax" data-rellax-speed="3" data-rellax-percentage="0.5" style="transform: rotate(45deg);">
-
-																</div>
-															</div>
-														</div>
-													</div>
-												</div>
-
-												<div class="copy" style="margin-top: 200px;margin-bottom: 500px">
-													<div class="container">
-														<div class="row">
-															<div class="col-md-offset-1 col-md-6">
-															</div>
-														</div>
-
-														<div class="row" style="margin-top: 140px">
-															<div class="col-md-offset-5 col-md-6">
-															</div>
-														</div>
-													</div>
-												</div>
-											</section>
-
-											<section id="contact" class="page page-section contact">
-												<div class="row">
-													<div class="col-md-offset-5 col-md-6">
-														<h1>CONTACT</h1>
-													</div>
-												</div>
-
-												<!-- CONTACT FORM -->
-												<div class="row">
-													<div class="col-md-offset-2 col-md-8">
-														<div class="text-editor">
-															<code>
-																<div class="row">
-																	<div class="col-md-offset-5 col-md-6">
-																		<form id="contact-form" method="POST" action="/php/mailer.php">
-																			<div class="form-group">
-																				<label for="name">NAME</label>
-																				<div class="input-group">
-																					<div class="input-group-addon">
-																						<i class="fa fa-user"></i>
-																					</div>
-																					<input class="form-control" type="text" name="name" id="name" placeholder="Your Name">
-																				</div>
-																			</div>
-																			<div class="form-group">
-																				<label for="email">EMAIL</label>
-																				<div class="input-group">
-																					<div class="input-group-addon">
-																						<i class="fa fa-envelope"></i>
-																					</div>
-																					<input class="form-control" type="email" name="email" id="email" placeholder="Your Email">
-																				</div>
-																			</div>
-																			<div class="form-group">
-																				<label for="subject">SUBJECT</label>
-																				<div class="input-group">
-																					<div class="input-group-addon">
-																						<i class="fa fa-pencil"></i>
-																					</div>
-																					<input class="form-control" type="text" name="subject" id="subject" placeholder="Subject">
-																				</div>
-																			</div>
-																			<div class="form-group">
-																				<label for="message">MESSAGE</label>
-																				<div class="input-group">
-																					<div class="input-group-addon">
-																						<i class="fa fa-comment"></i>
-																					</div>
-																					<textarea name="message" rows="5" id="message" class="form-control" placeholder="Your Message (1000 charaters max)"></textarea>
-																				</div>
-																			</div>
-
-																			<!-- GOOGLE RECAPTCHA -->
-																			<div class="g-recaptcha" data-sitekey="6LcnzSMUAAAAAH6mEC_q2lamxZoZPTXz3KqYu1oN"></div>
-
-																			<button class="btn btn-default" type="reset">RESET</button>
-																			<button class="btn btn-info" type="submit">SEND</button>
-																		</form>
-																	</div>
-																</div>
-															</code>
-														</div>
-													</div>
-												</div>
-										</div>
+								<div class="row" style="margin-top: 140px">
+									<div class="col-md-offset-5 col-md-6">
 									</div>
-								</section><!-- /CONTACT FORM-->
+								</div>
+							</div>
+						</div>
+					</section>
 
-				</main>
+					<section id="contact" class="page page-section contact">
+						<div class="row">
+							<div class="col-md-offset-5 col-md-6">
+								<h1>CONTACT</h1>
+							</div>
+						</div>
+
+						<!-- CONTACT FORM -->
+						<div class="row">
+							<div class="col-md-offset-2 col-md-8">
+								<div class="text-editor">
+									<code>
+										<div class="row">
+											<div class="col-md-offset-5 col-md-6">
+												<form id="contact-form" method="POST" action="/php/mailer.php">
+													<div class="form-group">
+														<label for="name">NAME</label>
+														<div class="input-group">
+															<div class="input-group-addon">
+																<i class="fa fa-user"></i>
+															</div>
+															<input class="form-control" type="text" name="name" id="name" placeholder="Your Name">
+														</div>
+													</div>
+													<div class="form-group">
+														<label for="email">EMAIL</label>
+														<div class="input-group">
+															<div class="input-group-addon">
+																<i class="fa fa-envelope"></i>
+															</div>
+															<input class="form-control" type="email" name="email" id="email" placeholder="Your Email">
+														</div>
+													</div>
+													<div class="form-group">
+														<label for="subject">SUBJECT</label>
+														<div class="input-group">
+															<div class="input-group-addon">
+																<i class="fa fa-pencil"></i>
+															</div>
+															<input class="form-control" type="text" name="subject" id="subject" placeholder="Subject">
+														</div>
+													</div>
+													<div class="form-group">
+														<label for="message">MESSAGE</label>
+														<div class="input-group">
+															<div class="input-group-addon">
+																<i class="fa fa-comment"></i>
+															</div>
+															<textarea name="message" rows="5" id="message" class="form-control" placeholder="Your Message (1000 charaters max)"></textarea>
+														</div>
+													</div>
+
+													<!-- GOOGLE RECAPTCHA -->
+													<div class="g-recaptcha" data-sitekey="6LcnzSMUAAAAAH6mEC_q2lamxZoZPTXz3KqYu1oN"></div>
+
+													<button class="btn btn-default" type="reset">RESET</button>
+													<button class="btn btn-info" type="submit">SEND</button>
+												</form>
+											</div>
+										</div>
+									</code>
+								</div>
+							</div>
+						</div>
+				</div>
+			</div>
+		</section><!-- /CONTACT FORM-->
+
+		</main>
 	</body>
 </html>
